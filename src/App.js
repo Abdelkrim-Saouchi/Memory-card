3f9e6bf0eaf08e9d7cd9acbb7cfacfcab8937181
Krimothiazine <krimouv1995@gmail.com>
2023-06-11 09:39:53 +0100

    Create Main component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 import Loading from './components/Loading';
 import Header from './components/Header';
+import Main from './components/Main';
 
 function App() {
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
+
   return (
     <div className="App">
-      {/* <Loading /> */}
-      <Header />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <Header />
+          <Main />
+        </>
+      )}
+      {/* <Loading />
+      <Header /> */}
     </div>
   );
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+const Main = () => {
+  return (
+    <main>
+      <p className="instruction">
+        *click card to gain points but not more than once per card
+      </p>
+      <div className="cards_container"></div>
+    </main>
+  );
+};
+
+export default Main;
